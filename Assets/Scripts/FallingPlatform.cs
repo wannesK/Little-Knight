4d4b6a917f068c6bbbacaa7f2b9b51193e6d71e3
wannesK <66689660+wannesK@users.noreply.github.com>
2021-01-29 13:23:02 +0300

    Falling Platform added

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 1);
+        }
+    }
+    void DropPlatform()
+    {
+        rigid.isKinematic = false;
+    }
+}
