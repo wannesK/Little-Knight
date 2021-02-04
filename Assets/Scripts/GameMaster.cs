fdda5ba2d63ea8376b7226593ba5a10ab93b7382
wannesK <66689660+wannesK@users.noreply.github.com>
2021-02-04 15:05:55 +0300

    Coin collection system

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -19,4 +19,5 @@ public class GameMaster : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI coinText;
+    int coin;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChanceCoin(int coinValue)
+    {
+        coin += coinValue;
+        coinText.text = coin.ToString();
+    }
+}
