66767ba88f83bd91bc45a65a553b45ab7c890b19
wannesK <66689660+wannesK@users.noreply.github.com>
2021-02-05 13:28:20 +0300

    Boss health bar

---
+++ Assets/Scripts/Enemy/BossHealtbar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealtbar : MonoBehaviour
+{
+    public Slider healtBar;
+    public EnemyHealt enemyHealt;
+
+    void Update()
+    {
+        healtBar.value = enemyHealt.healt;
+    }
+}
