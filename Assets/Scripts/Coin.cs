6eca6e86004c78ffde5e32e59416d03cb991eaba
wannesK <66689660+wannesK@users.noreply.github.com>
2021-02-08 15:48:23 +0300

    Coin save system created

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int coinValue = 5;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Destroy(this.gameObject);
-            ScoreManager.instance.ChanceCoin(coinValue);
+            ScoreManager.instance.CoinCounter();
         }
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,24 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using TMPro;
-using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
-{
+{  
     public static ScoreManager instance;
     public TextMeshProUGUI coinText;
-    int coin;
+    public GameData data;
+
+    private BinaryFormatter binaryFormatter;
+    private string saveFile;
     private void Awake()
     {
         if (instance == null)
-        {
             instance = this;
+        binaryFormatter = new BinaryFormatter();
+        saveFile = Application.persistentDataPath + "/game.data";
+    }
+
+    public void Start()
+    {
+        LoadGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            DeleteData();
+            Debug.Log("Coins Deleted");
         }
     }
 
-    public void ChanceCoin(int coinValue)
+    public void CoinCounter()
     {
-        coin += coinValue;
-        coinText.text = coin.ToString();
+        data.coin += Random.Range(3,8);
+        coinText.text = "" + data.coin;
+    }
+
+    public void SaveData()
+    {
+        FileStream fileStream = new FileStream(saveFile, FileMode.Create);
+        binaryFormatter.Serialize(fileStream, data);
+        fileStream.Close();
+    }
+
+    public void LoadData()
+    {
+        if (File.Exists(saveFile))
+        {
+            FileStream fileStream = new FileStream(saveFile, FileMode.Open);
+            data = (GameData)binaryFormatter.Deserialize(fileStream);
+            coinText.text = " " + data.coin;
+            fileStream.Close();
+        }
+
+    }
+
+    public void DeleteData()
+    {
+        FileStream fileStream = new FileStream(saveFile, FileMode.Create);
+        data.coin = 0;
+        coinText.text = "0";
+        binaryFormatter.Serialize(fileStream, data);
+        fileStream.Close();
+    }
+
+    private void OnEnable()
+    {
+        LoadData();
+    }
+    private void OnDisable()
+    {
+        SaveData();
+    }
+
+    public void LoadGame()
+    {
+        if (data.firstLoading)
+        {
+            data.coin = 0;
+            data.firstLoading = false;
+        }
     }
 }
+[System.Serializable] 
+public class GameData
+{
+    public int coin;
+    public bool firstLoading;
+}
+
