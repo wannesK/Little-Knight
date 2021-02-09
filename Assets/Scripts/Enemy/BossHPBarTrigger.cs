4d8b8334f979ffaf1af302e4d416fac69b6460bb
wannesK <66689660+wannesK@users.noreply.github.com>
2021-02-09 12:42:17 +0300

    Level_3 changes

--- Assets/Scripts/Enemy/BossHPBarTrigger.cs
+++ Assets/Scripts/Enemy/BossHPBarTrigger.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BossHPBarTrigger : MonoBehaviour
 {
     public GameObject bossHPBar;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
--- Assets/Scripts/Enemy/BossHealtbar.cs
+++ Assets/Scripts/Enemy/BossHealtbar.cs
@@ -8,8 +8,17 @@ public class BossHealtbar : MonoBehaviour
     public Slider healtBar;
     public EnemyHealt enemyHealt;
 
+    private LevelEndTrigger end;
+    private void Start()
+    {
+        end = GameObject.FindGameObjectWithTag("LevelEnd").GetComponent<LevelEndTrigger>();
+    }
     void Update()
     {
         healtBar.value = enemyHealt.healt;
+        if (enemyHealt.healt <= 0)
+        {
+            end.LevelComplete();
+        }
     }
 }
