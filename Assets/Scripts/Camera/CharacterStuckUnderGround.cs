ce1ba3dc0279e547a9e9ee2659503465ec2a9338
wannesK <66689660+wannesK@users.noreply.github.com>
2021-03-08 14:05:43 +0300

    Bug fixed - Character get stuck under water

---
+++ Assets/Scripts/Camera/CharacterStuckUnderGround.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterStuckUnderGround : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
